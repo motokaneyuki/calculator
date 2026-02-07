d19b5f18e201e1f5bc22c9f8956e32e7655f1db7
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-02-07 18:23:26 +0900

    feat: add operate function for math routing

--- scripts.js
+++ scripts.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, firstNumber, secondNumber){
+    switch(operator) {
+        case 'sum':
+            return sum(firstNumber, secondNumber);
+        case 'subtract':
+            return subtract(firstNumber, secondNumber);
+        case 'multiply':
+            return multiply(firstNumber, secondNumber);
+        case 'divide':
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
