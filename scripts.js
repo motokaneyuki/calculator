4d1998ec337af02019bd7d63caaab18508217602
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-02-08 02:56:35 +0900

    feat: switch to second number on operator click

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const digitButtons = document.querySelectorAll('.digits');
+const operatorButtons = document.querySelectorAll('.operators');
 const answer = document.querySelector('.answer');
 
 function sum(a, b) {
@@ -32,17 +33,56 @@ function operate(operator, firstNumber, secondNumber){
 
 let expression = {
     firstNumber: null,
-    operator: null,
+    operator: '',
     secondNumber: null,
 }
 
-digitButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let getNumber = e.target.textContent;
-        console.log(getNumber);
-        answer.textContent += getNumber;
+// to get the first number
+function firstListener(event){
+    let getNumber = event.target.textContent;
+    console.log(getNumber);
+    answer.textContent += getNumber;
 
-        expression.firstNumber = Number(answer.textContent);
-        console.log(expression.firstNumber);
+    expression.firstNumber = Number(answer.textContent);
+    console.log(expression.firstNumber);  
+}
+
+digitButtons.forEach(button => {
+    button.addEventListener('click', firstListener);
+})
+
+// to get the operator
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let getOperator = e.target.textContent;
+        console.log(getOperator);
+        expression.operator = getOperator;
+        console.log(expression.operator);
+
+        // to keep the first number as is
+        digitButtons.forEach(button => {
+            button.removeEventListener('click', firstListener);
+        })
+
+        let tempFirstNumber = expression.firstNumber;
+        console.log("temp", tempFirstNumber);
+    
+        // to get the second number
+        digitButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                if (expression.secondNumber === null){
+                    answer.textContent = '';
+                }
+                
+                let getNumber = e.target.textContent;
+                console.log(getNumber);
+                answer.textContent += getNumber;
+
+                expression.secondNumber = Number(answer.textContent);
+                expression.firstNumber = tempFirstNumber;
+                console.log("exp", expression);
+            })
+        })    
+
     })
 })
